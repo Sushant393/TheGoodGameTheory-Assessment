645bce546a9ec5fb7ec18cbf5ae034bb43c0137d
Sushant Kr Shah Kanu <54392214+Sushant393@users.noreply.github.com>
2023-09-25 18:43:32 +0530

    some basic changes

--- src/App.jsx
+++ src/App.jsx
@@ -33,10 +33,10 @@ const searchResults = (e)=>{
   return (
     <div className=''>
       
-    <div className="pt-10  container flex flex-col items-center">
+    <div className="pt-10  container flex flex-col items-center ">
     <input type="search" name="search" placeholder='search beer...' id="" className=' rounded-lg border-2 text-xl outline-none p-2 ' onChange={e=>searchResults(e)} />
     <div className="mt-10 card-container flex justify-evenly gap-5 items-center flex-wrap ">
-        {filteredData.map((beer)=>{
+        {filteredData?.map((beer)=>{
        return <BeerCard key={beer.id} beer={beer}/>
       })}
     </div>
--- src/components/BeerCard.jsx
+++ src/components/BeerCard.jsx
@@ -4,19 +4,17 @@ const BeerCard = ({beer}) => {
    
    
   return (
-    <div className="card max-w-[300px] flex justify-center items-center  gap-5 ">
+    <div className="card max-w-[320px] h-[200px] flex justify-center items-center  gap-5 border-2 border-slate-300 rounded-lg p-5">
         <div className="image h-[150px] w-[150px]">
             <img src={beer.image_url} alt="" className='h-full w-[100px] object-contain'/>
         </div>
-        <div className="name w-2/3 ">
+        <div className="name  ">
             <div className="names">
-            <h1 className=' whitespace-nowrap'>{beer.name}</h1>
-                <p>{beer.tagline}</p>
-                <span>Since-{beer.first_brewed}</span>
+            <h1 className=' text-xl font-bold '>{beer.name}</h1>
+                <p className='mt-2 text-sm'>{beer.tagline}</p>
+                <span className='mt-2 text-sm'>Since-{beer.first_brewed}</span>
             </div>
-                
-                 
-                
+
             </div>
 
     </div>
